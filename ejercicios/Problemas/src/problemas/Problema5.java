79c1ad462f935684f36285d0be733e851fb471d8
Pablo Ordóñez <pablosebas61@gmail.com>
2025-01-09 18:18:13 -0500

    Cambios ejercicios

--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,31 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+
+        int[][] resultado = sumarMatrices(informacion, informacion2);
+
+        for (int i = 0; i < resultado.length; i++) {
+            for (int j = 0; j < resultado[i].length; j++) {
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] sumarMatrices(int[][] matriz1, int[][] matriz2) {
+        int filas = matriz1.length;
+        int columnas = matriz1[0].length;
+        int[][] suma = new int[filas][columnas];
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                suma[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+
+        return suma;
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,39 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
-    
+  
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+
+        double media = calcularMedia(informacion);
+        double desviacionEstandar = calcularDesviacion(informacion, media);
+
+        System.out.printf("La media aritmética es: %.2f\n", media);
+        System.out.printf("La desviación estándar es: %.2f\n", 
+                desviacionEstandar);
+    }
+
+    public static double calcularMedia(int[] arreglo) {
+        int suma = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+
+        return (double) suma / arreglo.length;
+    }
+
+    public static double calcularDesviacion(int[] arreglo, double media) {
+        double sumaCuadrados = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            sumaCuadrados = sumaCuadrados + Math.pow(arreglo[i] - media, 2);
+        }
+
+        double varianza = sumaCuadrados / arreglo.length;
+        return Math.sqrt(varianza);
+    }
 }
+
+
+
