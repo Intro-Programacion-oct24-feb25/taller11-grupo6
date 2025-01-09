fd2be1c17605f52db3a27999c70ad9962a389a03
Jorge Granda <jorjorgeandgranda06@gmail.com>
2025-01-09 12:22:20 -0500

    Problema 4

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+import java.util.Locale;
 /**
  *
  * @author reroes
@@ -16,6 +18,56 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+
+        double nota1;
+        double nota2;
+        double nota3;
+        double nota4;
+
+        System.out.println("Ingrese el valor de la nota 1:");
+        nota1 = entrada.nextDouble();
+
+        System.out.println("Ingrese el valor de la nota 2:");
+        nota2 = entrada.nextDouble();
+
+        System.out.println("Ingrese el valor de la nota 3:");
+        nota3 = entrada.nextDouble();
+
+        System.out.println("Ingrese el valor de la nota 4:");
+        nota4 = entrada.nextDouble();
+
+        String promedioCualitativo = funcionPromedio(nota1, nota2, nota3,
+                nota4);
+        String mensaje = funcionMayusculas("El promedio de las notas es:" + nota1
+                + "," + nota2 + "," + nota3 + "," + nota4 + promedioCualitativo);
+
+        System.out.println(mensaje);
     }
-    
+
+    public static String funcionPromedio(double nota1, double nota2, double 
+            nota3, double nota4) {
+        double promedio = (nota1 + nota2 + nota3 + nota4) / 4;
+        if (promedio >= 0 && promedio <= 5) {
+            return " regular";
+        } else {
+            if (promedio > 5 && promedio <= 8) {
+                return " bueno";
+            } else {
+                if (promedio > 8 && promedio <= 9) {
+                    return " Muy bueno";
+                } else {
+                    if (promedio > 9 && promedio <= 10) {
+                        return " Sobresaliente";
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    public static String funcionMayusculas(String mensaje){
+        return mensaje.toUpperCase();
+    }
 }
